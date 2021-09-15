0dca90e1931d52494a287b06d8c38bb73a841b98
MV | Clappy <70108843+MV-Clappy@users.noreply.github.com>
2021-09-15 08:35:27 +0100

    Update ready.js

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -1,5 +1,5 @@
 const Event = require("../Structures/Event.js");
 module.exports = new Event("ready", client => {
 		console.log("I HATE MY LIFE!")
-		client.user.setActivity("Over Mah-frands!", { type: "WATCHING",})
+		client.user.setActivity("Made by Clappy", { type: "WATCHING",})
 });
